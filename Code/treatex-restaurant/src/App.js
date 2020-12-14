c350fbe219af3fc52dce0d3f09b055768e1f8aaf
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2020-12-14 12:28:30 +0600

    #3  Component Tree Added
    Component tree added for the project

--- Code/treatex-restaurant/src/App.js
+++ Code/treatex-restaurant/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainConponent from './components/MainComponent';
+
+import MainComponent from './components/MainComponent'
 
 function App() {
   return (
     <div className="App">
+      <MainConponent/>
       </div>
   );
 }
