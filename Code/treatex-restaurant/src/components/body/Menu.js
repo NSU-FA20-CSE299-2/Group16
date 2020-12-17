cde6bc87f88a47873a450536db7b0cd298f97c97
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2020-12-17 19:39:49 +0600

    Enhancement #4  Created Menu Component
    Created MenuItems, link dishdetail to menu and linked Menuitems to the body component

--- Code/treatex-restaurant/src/components/body/Menu.js
+++ Code/treatex-restaurant/src/components/body/Menu.js
@@ -1,16 +1,26 @@
 import React,{Component} from 'react';
 import DISHES from '../../data/dishes'
+import MenuItem from './MenuItem'
 
-class menu extends Component {
+class Menu extends Component {
     state = {
         dishes : DISHES
     }
     render(){
+        const menu = this.state.dishes.map(item =>{
+            return(
+                <div>
+                    <MenuItem dish = {item} key={item.id}/>
+                </div>
+
+            );
+        })
+       
         return(
             <div className = 'container'>
                 <div className="row">
                     <div className="col-6">
-
+                        {menu}
                     </div>
                 </div>
             </div>
@@ -18,4 +28,4 @@ class menu extends Component {
         )
     }
 }
-export default Menu
+export default Menu;
