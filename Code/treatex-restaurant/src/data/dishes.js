4be2064c9b419cdd6feab279b87ed92692a280f0
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2020-12-17 18:41:26 +0600

    Enhancement #4  Added Dish structure
    Added basic dish details structure with id,name,label,details and comment

---
+++ Code/treatex-restaurant/src/data/dishes.js
@@ -0,0 +1,35 @@
+const DISHES = [
+    {
+        id : 0,
+        name : 'Ginger Burger',
+        image : 'assets/images/0burger.jpg',
+        label : 'Hot',
+        price : '299',
+        description: 'This chicken burger gets an Asian-flavor twist with ginger and garlic. It\'s topped off with a fabulous coleslaw. If you like, serve the coleslaw as a side for grilled items, like chicken breasts, fish fillets or chops',
+        comments : [
+            {
+                id: 0,
+                rating: 5,
+                comment : "Imagine all the eatables, living in conFusion!",
+                author: 'Sany',
+                date: '2020-12-17T17:57:28.556094Z'
+            },
+            {
+                id: 1,
+                rating: 4.5,
+                comment : "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: 'Turza',
+                date: '2020-11-17T18:57:28.556094Z'
+            },
+            {
+                id: 2,
+                rating: 4.5,
+                comment: "Eat it, just eat it!",
+                author: "Fatema",
+                date: "2020-08-13T17:57:28.556094Z"
+            },     
+        ]
+    }
+]
+
+export default DISHES;
