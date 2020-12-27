1bcb0369a650bf1fc004228ae34c401a2e1d6de2
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2020-12-27 18:13:30 +0600

    Enhancement #6 : Router Path Added
    Routher Path Added to the nav Items

--- Code/treatex-restaurant/src/components/body/Body.js
+++ Code/treatex-restaurant/src/components/body/Body.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Route,Redirect} from 'react-router-dom'
 import Menu from './Menu'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 const Body = ()=>{
     return (<div>
+        <Route path ="/" exact component={Home}/>
+        <Route path ="/menu" exact component={Menu}/>
+        <Route path ="/contact" exact component={Contact}/>
+        <Route path ="/about" exact component={About}/>
 
-    <Menu />
 
     </div>
     );
