524be928d39544746dfa083ca362751c9e69378f
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2020-12-30 19:16:03 +0600

    Enhancement #11 :Footer Added
    Basic footer with copyright is added

--- Code/treatex-restaurant/src/components/footer/Footer.js
+++ Code/treatex-restaurant/src/components/footer/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Footer = ()=>{
-    return (<div>
+    return (<div className="footer">
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-auto">
+                    <p className="footer-text"><b>Treatex Reastaurant &#169; Copyright 2020 </b> </p>
+                </div>
+            </div>
+            
+        </div>
 
     </div>
     );
