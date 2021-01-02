9a93545642308ce05ca07bcbeb0b39dabcee1551
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2021-01-03 01:12:36 +0600

    Enhancement #14 : Reducer Added with initial state
    Reducer component added to the app with initial state

---
+++ Code/treatex-restaurant/src/redux/reducer.js
@@ -0,0 +1,10 @@
+import DISHES from '../data/dishes'
+import COMMENTS from '../data/Comments'
+const initialState = {
+    dishes : DISHES,
+    comments: COMMENTS,
+}
+
+const reducer = (state,action)=>{
+    return state 
+}
