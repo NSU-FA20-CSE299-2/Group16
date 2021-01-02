87d4230502d613e0b766616043e7774cf8cfe007
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2021-01-02 19:59:38 +0600

    Enhancement #8 : Slider Structure added
    Slider structure to the home page is added. Images will be added later

--- Code/treatex-restaurant/src/components/body/Home.js
+++ Code/treatex-restaurant/src/components/body/Home.js
@@ -1,12 +1,14 @@
 import React,{Component} from  'react'
+import { UncontrolledCarousel } from 'reactstrap';
+import Slides from './Slides'
 
 class Home extends Component {
     render(){
         document.title = "Treatex-Home"
         return(
-
-            <div>
-            </div>
+            <UncontrolledCarousel items={Slides} />
+       
+           
         )
     }   
 }
---
+++ Code/treatex-restaurant/src/components/body/Slides.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Slides = [
+  {
+    src: "",
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Slide 1 Header',
+    key: '1'
+  },
+  {
+    src:"",
+	altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Slide 2 Header',
+    key: '2'
+  },
+  {
+    src: "assets/images/0burger.jpg",
+	altText: 'Slide 4',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header',
+    key: '3'
+  }
+];
+
+export default Slides;
