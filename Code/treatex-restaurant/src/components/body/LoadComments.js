8daf2102990f12087244420cf7d274d9cc622c65
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2021-01-02 19:51:23 +0600

    Enhancement #5 : Comment Style updated
    View comment style updated

--- Code/treatex-restaurant/src/components/body/LoadComments.js
+++ Code/treatex-restaurant/src/components/body/LoadComments.js
@@ -8,15 +8,12 @@ const LoadComments = props =>{
                 <div key={comment.id}>
                     <h5>
                         {comment.author}
+                    </h5>
                         <p>{comment.comment}</p>
                         <p>{dateformat(comment.date,"dd mmm-yyyy")}</p>
-                    </h5>
-
                 </div>
             )
         })
-
-
     )
 }
 export default LoadComments
