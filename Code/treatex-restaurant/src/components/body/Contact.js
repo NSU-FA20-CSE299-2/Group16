915037833aaadacf3820ec9e9e66388017d629ab
SanyMKhaled <73827409+SanyMKhaled@users.noreply.github.com>
2021-01-12 14:02:52 +0600

    Enhancement #29 : Reset Input Field
    Input field data resets after submit

--- Code/treatex-restaurant/src/components/body/Contact.js
+++ Code/treatex-restaurant/src/components/body/Contact.js
@@ -18,6 +18,7 @@ const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 class Contact extends Component {
     handleSubmit = values => {
         console.log(values);
+        this.props.resetFeedbackForm()
     }
     render(){
         document.title = "Treatex-Contact"
